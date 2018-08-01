8d58bf5f5906b74b6a45a0c534f244f846089b70
root <root@snerp2.c.stable-splicer-183522.internal>
2018-08-01 00:37:15 +0000

    modified transaction py

--- it_dept_library/it_dept_library/doctype/it_dept_library_transaction/it_dept_library_transaction.py
+++ it_dept_library/it_dept_library/doctype/it_dept_library_transaction/it_dept_library_transaction.py
@@ -12,12 +12,17 @@ class ITDeptLibraryTransaction(Document):
                 "date": ("<=", self.date),
                 "name": ("!=", self.name)
             })
+        if self.type=="Block":
+            return
         if self.type=="Issue":
             msg = _("Book {0} {1} has not been returned since {2}")
             if last_transaction and last_transaction[0].type=="Issue":
                 frappe.throw(msg.format(self.book, self.book_name,
                     last_transaction[0].date))
+       
         else:
             if not last_transaction or last_transaction[0].type!="Issue":
-                frappe.throw(_("Cannot return book not issued"))
+                frappe.throw(_("Cannot return book as it is not issued"))
+     
+
 
